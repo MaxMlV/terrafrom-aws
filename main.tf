72d33eeb577bc27c999e9e028f23716f3bad59f9
MaxIAM-at-519405919099 <max.malphil@gmail.com>
2022-05-12 20:35:02 +0300

    added ec2 instance on ubuntu

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "dev_instance" {
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.server_ami.id
+  key_name               = aws_key_pair.mtc_auth.key_name
+  vpc_security_group_ids = [aws_security_group.mtc_sg.id]
+  subnet_id              = aws_subnet.mtc_public_subnet.id
+  user_data              = file("userdata.tpl")
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  tags = {
+    Name = "dev-instance"
+  }
+}
