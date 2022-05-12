3a66a6f47081f4d102f00475652f5864de992e46
MaxMlV <max.malphil@gmail.com>
2022-05-12 21:11:46 +0300

    deleted ssh-auth.tf

--- ssh-auth.tf
+++
@@ -1,8 +0,0 @@
-# At this step I'll specify key pair from local path,
-# but as soon as I'll be working with GitHub Actions it'll be added as a secret
-# and configured from the pipeline.
-
-resource "aws_key_pair" "mtc_auth" {
-  key_name   = "mtckey"
-  public_key = file("~/.ssh/mtckey.pub")
-}
