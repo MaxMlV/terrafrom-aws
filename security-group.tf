b95e2e9bb6c655b30faee81b3c8059e17174aa97
MaxIAM-at-519405919099 <max.malphil@gmail.com>
2022-05-12 17:27:17 +0300

    added security group

---
+++ security-group.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "mtc_sg" {
+  name        = "dev-sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.mtc_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
